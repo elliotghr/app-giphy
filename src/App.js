aeaa5273cf9a9327b7707914ebc72e754246dc03
elliotghr <gunsnroses.2617@gmail.com>
2023-02-16 18:03:25 -0600

    App Giphy creada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import getGifs from "./services/getGif";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    getGifs({ keyword: "morty" }).then((res) => setGifs(res));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-header">
+        {gifs.map((el) => (
+          <img src={el.url} alt="giphy"></img>
+        ))}
+      </section>
     </div>
   );
 }
---
+++ src/services/getGif.js
@@ -0,0 +1,16 @@
+const api_key = "I5CtQc1XVkHMhZg0ys5ePE0OS1VbZ7hr";
+
+export default function getGifs({ keyword = "panda" } = {}) {
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
+  return fetch(apiURL)
+    .then((res) => res.json())
+    .then((response) => {
+      const { data } = response;
+      const gifs = data.map((el) => {
+        const { title, id } = el;
+        const { url } = el.images.downsized_medium;
+        return { title, id, url };
+      });
+      return gifs;
+    });
+}
